706d99bbc09874a28e0d4dbe46dbd1b73ce93d45
zoza99 <86885431+zoza99@users.noreply.github.com>
2022-06-25 14:40:56 +0300

    delete 2

--- main.js
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <button name="next-button">Obi-Wan abandons the high ground to salute you</button>
-        <script type="module" src="Новые свечки(f).html"></script>
-        
-    </body>
-</html>
